7d5e71a52f1ef6df90ada64403296a67bf6a7e8e
JoseCarlosPereiraNeto <jose11carlos05@gmail.com>
2025-03-10 15:49:20 -0300

    Update data.js

---
+++ src/config/data.js
@@ -0,0 +1,14 @@
+require('dotenv').config()
+
+const knex = require('knex')({
+        client: 'mysql2',
+        connection: {
+        host: process.env.HOST,
+        port: 3306,
+        user: process.env.USER,
+        password: process.env.PASSWORD,
+        database: process.env.DATABASE,
+    },
+  });
+
+module.exports = knex;

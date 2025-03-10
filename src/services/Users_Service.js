8b1d70146fd649823bf9637905b7d1937610c3e9
JoseCarlosPereiraNeto <jose11carlos05@gmail.com>
2025-03-10 15:49:39 -0300

    Create Users_Service.js

---
+++ src/services/Users_Service.js
@@ -0,0 +1,22 @@
+// importar a conexão com o banco
+const knex = require('../config/data'); // é a ponte que liga este arquivo ao banco de dados
+
+
+class UsersService
+{
+    // criar um método para buscar todos os usuários
+    // vamos impedir o código de continuar até que as informações sejam retornada
+    async findAll()
+    {
+        try
+        {
+            let users = await knex.select(["id","name","email"]).table("users");
+            console.log(users);
+        } catch (error)
+        {
+            console.log(error);
+        }
+    }
+}
+
+module.exports = new UsersService();

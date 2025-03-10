d6f16cab5527a84c773dc06ac18039aaa3b85c7d
aspiringluke <l.paulino@sou.unifeob.edu.br>
2025-03-10 16:16:42 -0300

    fix: Erro na conexão com o banco consertado
    haviam alguns erros na conexão com o banco de dados dentro da classe UsersService e outros erros no arquivo de configuração

--- src/config/data.js
+++ src/config/data.js
@@ -3,11 +3,11 @@ require('dotenv').config()
 const knex = require('knex')({
         client: 'mysql2',
         connection: {
-        host: process.env.HOST,
+        host: process.env.DB_HOST,
         port: 3306,
-        user: process.env.USER,
-        password: process.env.PASSWORD,
-        database: process.env.DATABASE,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_DATABASE,
     },
   });
 

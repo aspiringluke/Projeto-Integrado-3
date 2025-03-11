cc73b8bca7b465c4ad3081d1801458a3d272d185
aspiringluke <l.paulino@sou.unifeob.edu.br>
2025-03-11 16:07:09 -0300

    feat: Teste de divisão de rotas

--- src/routes/router.js
+++ src/routes/router.js
@@ -4,13 +4,12 @@ const express = require('express');
 // utilizar o método de rotas do express
 const router = express.Router();
 
-// requerer o método de listar todos
-const ListAllUser = require('../controllers/users/list_all_users');
+const userRoute = require('./userRoute')
 
 // rota url
 // rota de listar os usuários
 // existem padrões para definir os nomes nas URLs
 // para listar os usuários, é 'users'
-router.get('/users', ListAllUser.listAll);
+router.use('/users', userRoute);
 
 module.exports = router;

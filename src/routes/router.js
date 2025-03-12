72f83408df5b0591dd873a9a25ba453b325f5abe
aspiringluke <l.paulino@sou.unifeob.edu.br>
2025-03-12 14:33:23 -0300

    feat: Rota dos pedidos adicionada

--- src/routes/router.js
+++ src/routes/router.js
@@ -4,13 +4,15 @@ const express = require('express');
 // utilizar o método de rotas do express
 const router = express.Router();
 
-const userRoute = require('./userRoute')
+const userRoute = require('./userRoute');
+const pedidoRoute = require('./pedidoRoute');
 
 // rota url
 // rota de listar os usuários
 // existem padrões para definir os nomes nas URLs
 // para listar os usuários, é 'users'
 router.use('/users', userRoute);
+router.use('/pedidos', pedidoRoute);
 
 // TODO: Criar as rotas GET dos outros dois arquivos
 

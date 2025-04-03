d6a33eca7f1cdd7fa62159bfd98ccee78243d34e
aspiringluke <l.paulino@sou.unifeob.edu.br>
2025-04-03 14:46:53 -0300

    refactor: Removidas rotas presentes lugar errado
    As rotas com ID estavam no arquivo principal router.js,
    mas não deveriam estar lá

--- src/routes/router.js
+++ src/routes/router.js
@@ -1,26 +1,13 @@
-// configuração de rotas?
-// requerer o express
 const express = require('express');
-// utilizar o método de rotas do express
 const router = express.Router();
 
 const userRoute = require('./userRoute');
 const pedidoRoute = require('./pedidoRoute');
 const produtoRoute = require('./produtoRoute');
 
-// rota url
-// rota de listar os usuários
-// existem padrões para definir os nomes nas URLs
-// para listar os usuários, é 'users'
+// redirecionamento de rotas
 router.use('/users', userRoute);
 router.use('/pedidos', pedidoRoute);
 router.use('/produtos', produtoRoute);
-
-//rota para listar apenas 1 usuario , produto ou pedido.
-router.use('/:id', pedidoRoute);
-router.use('/:id', userRoute);
-router.use('/:id', produtoRoute);
-
-// TODO: Criar as rotas GET dos outros dois arquivos
 
 module.exports = router;

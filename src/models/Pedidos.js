b4aa85b0e2233b7e14a91b58946372cf72e82e6c
JoseCarlosPereiraNeto <jose11carlos05@gmail.com>
2025-04-17 16:52:57 -0300

    feet  post pediddos

--- src/models/Pedidos.js
+++ src/models/Pedidos.js
@@ -37,12 +37,23 @@ class Pedidos
     async create(newPedido)
     {
         try {
-            await knex
+            let idpedido = await knex
                 .insert({
                     status: `${newPedido.status}`,
                     data: `${newPedido.data}`,
-                })
+                    idUsuario: newPedido.idUsuario,
+                    idCliente: newPedido.idCliente
+                }, ['idPedido'])
                 .into('pedidos');
+            
+            console.log(idpedido);
+            
+            let result = this.insertItems(idpedido, newPedido.itens);
+
+            return result.valid
+            ? {valid: true, message: result.message}
+            : {valid: false, error: result.error};
+
             return {valid: true};
         } catch (error) {
             console.log(error);
@@ -50,13 +61,27 @@ class Pedidos
         }
     }
 
-    async insertItems(itens)
+    async insertItems(idpedido, itens)
     {
-        for(const item of itens){
+        try{
+            for(const item of itens){
+                await knex('itenspedido')
+                        .insert({
+                            idPedido: idpedido,
+                            idProduto: item.idProduto,
+                            quantidade: item.quantidade,
+                            valorCombinado: item.valorCombinado
+                        });
+            }
 
+            return {valid: true, message: "Itens cadastrados."};
         }
+        catch(error){
+            return {valid:false, error:error};
+        }
+
     }
-
-
 }
+
+
 module.exports = new Pedidos();

2eb134636614b588ef62b51cce8b606201fecbda
aspiringluke <l.paulino@sou.unifeob.edu.br>
2025-05-01 16:51:24 -0300

    feat: Implementado suporte a CORS

--- src/api.js
+++ src/api.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const api = express();
 const router = require('./routes/router');
+const cors = require('cors');
 
+api.use(cors());
 api.use(express.urlencoded({extended:false}));
 api.use(express.json());
 api.use('/', router);

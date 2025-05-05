bed78e5af081a7c9d460a61d8654c8bf39c28f9e
aspiringluke <l.paulino@sou.unifeob.edu.br>
2025-05-05 15:27:34 -0300

    refactor: Mensagem de inicialização alterada

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
-require('dotenv').config()
-
+require('dotenv').config();
 const api = require('./src/api')
 
 api.listen(process.env.PORT,()=>{
-    console.log('\n API INICIALIZADA')
+    console.log(`\n API rodando na porta ${process.env.PORT}`)
 })

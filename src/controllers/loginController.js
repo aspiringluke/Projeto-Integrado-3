52799cca8681c71e6c6f590489adb0c40745cbd7
aspiringluke <l.paulino@sou.unifeob.edu.br>
2025-05-24 10:51:51 -0300

    fix: ID função adicionado ao token no login

--- src/controllers/loginController.js
+++ src/controllers/loginController.js
@@ -23,7 +23,7 @@ class LoginController
 
         let isPasswordCorrect = await comparePasswordService(senha, user.values.senha);
         if(isPasswordCorrect){
-            let token = jwt.sign({idUsuario: user.values.idUsuario, email: email}, process.env.SECRET, {expiresIn: 1440000});
+            let token = jwt.sign({idUsuario: user.values.idUsuario, email: email, role: user.values.Funcao_idFuncao}, process.env.SECRET, {expiresIn: 1440000});
             res.status(200).json({success: true, token: token, funcaoId: user.values.Funcao_idFuncao, userId: user.values.idUsuario});
         }
         else{

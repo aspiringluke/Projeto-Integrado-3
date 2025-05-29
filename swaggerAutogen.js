46309a2d20492ccdf3648768280a4ae4f1b085a8
aspiringluke <l.paulino@sou.unifeob.edu.br>
2025-05-29 15:12:23 -0300

    Squashed commit of the following:
    commit 9e9b1d37862f165136b68ed79b828b0083030565
    Author: aspiringluke <l.paulino@sou.unifeob.edu.br>
    Date:   Thu May 29 15:11:59 2025 -0300
    
        refactor: Local da rota do swagger alterada
    
    commit b7f517d1fd3953337daf54563ca3b2775a1d56af
    Author: aspiringluke <l.paulino@sou.unifeob.edu.br>
    Date:   Fri May 16 22:21:50 2025 -0300
    
        docs: Documentação swagger criada
    
    commit ea7ccf75d28666e200eee4146901af37887caa08
    Author: aspiringluke <l.paulino@sou.unifeob.edu.br>
    Date:   Fri May 16 22:21:37 2025 -0300
    
        package: swagger-autogen e swagger-ui-express adicionados

---
+++ swaggerAutogen.js
@@ -0,0 +1,22 @@
+const swaggerAutogen = require('swagger-autogen')();
+const outputFile = './swagger/swagger_output.json';
+const routes = ['./src/routes/router.js'];
+
+const doc = {
+    info: {
+        version: '1.0.0',
+        title: 'API de Cadastro de Pedidos',
+        description: 'Api de cadastro para a Argenzio de Casa Branca'
+    },
+    host: 'localhost:4040',
+    basePath: '/',
+    scheme: [],
+    consumes: [],
+    produces: [],
+    tags: [],
+    securityDefinitions: {},
+    definitions: {},
+    components: {}
+};
+
+swaggerAutogen(outputFile, routes, doc);
